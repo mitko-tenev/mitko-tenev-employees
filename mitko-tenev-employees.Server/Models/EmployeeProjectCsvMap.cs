1052ca80fa177f517af1cb9e410c192907d79c1f
mitaka53 <34245881+mitaka53@users.noreply.github.com>
2025-04-26 13:42:37 +0300

    Change parsing of DateTo field

--- mitko-tenev-employees.Server/Models/EmployeeProjectCsvMap.cs
+++ mitko-tenev-employees.Server/Models/EmployeeProjectCsvMap.cs
@@ -9,7 +9,13 @@
             Map(m => m.EmpID).Name("EmpID");
             Map(m => m.ProjectID).Name("ProjectID");
             Map(m => m.DateFrom).Name("DateFrom");
-            Map(m => m.DateTo).Name("DateTo").Optional();
+            Map(m => m.DateTo).Name("DateTo").Convert(args =>
+            {
+                // Check if the string value is "NULL" and convert accordingly
+                var value = args.Row.GetField("DateTo");
+
+                return string.IsNullOrEmpty(value) || value.Trim().ToUpper() == "NULL" ? (DateTime?)null : DateTime.Parse(value);
+            });
         }
     }
 }
