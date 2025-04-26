ff5565d1ecb3a05f0f5457d334716bd54e4136e2
mitaka53 <34245881+mitaka53@users.noreply.github.com>
2025-04-26 13:22:42 +0300

    Add CsvParserService

---
+++ mitko-tenev-employees.Server/Services/CsvParserService.cs
@@ -0,0 +1,44 @@
+﻿namespace mitko_tenev_employees.Server.Services
+{
+    using CsvHelper.Configuration;
+    using CsvHelper;
+    using mitko_tenev_employees.Server.Models;
+    using System.Globalization;
+    using mitko_tenev_employees.Server.Services.Interfaces;
+
+    public class CsvParserService : ICsvParserService
+    {
+        public List<EmployeeProject> ParseCsvFile(IFormFile file)
+        {
+            List<EmployeeProject> employeeProjects = new();
+
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+                Delimiter = ","
+            }))
+            {
+                csv.Context.RegisterClassMap<EmployeeProjectCsvMap>();
+                employeeProjects = csv.GetRecords<EmployeeProject>().ToList();
+            }
+
+            // Set NULL DateTo values to current date
+            SetCurentDateForRecordsWithEmptyDateTo(employeeProjects);
+
+            return employeeProjects;
+        }
+
+        private void SetCurentDateForRecordsWithEmptyDateTo(IEnumerable<EmployeeProject> employeeProjects)
+        {
+            var today = DateTime.Today;
+            foreach (var project in employeeProjects)
+            {
+                if (!project.DateTo.HasValue)
+                {
+                    project.DateTo = today;
+                }
+            }
+        }
+    }
+}
---
+++ mitko-tenev-employees.Server/Services/Interfaces/ICsvParserService.cs
@@ -0,0 +1,9 @@
+﻿namespace mitko_tenev_employees.Server.Services.Interfaces
+{
+    using mitko_tenev_employees.Server.Models;
+
+    public interface ICsvParserService
+    {
+        List<EmployeeProject> ParseCsvFile(IFormFile file);
+    }
+}
