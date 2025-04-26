bd9184555c5a960a2e61c2ce89270a1d9e93ef52
mitaka53 <34245881+mitaka53@users.noreply.github.com>
2025-04-26 22:56:00 +0300

    Add environments and load api url from envs

--- mitko-tenev-employees.client/src/app/employee-project/shared/employee-project.service.ts
+++ mitko-tenev-employees.client/src/app/employee-project/shared/employee-project.service.ts
@@ -2,19 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CommonProject } from '../../models/models';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeProjectService {
-  private apiUrl = '/api/EmployeeProject'; // TODO: Replace with your API endpoint
-
   constructor(private http: HttpClient) { }
 
   uploadFile(file: File): Observable<CommonProject[]> {
     const formData = new FormData();
     formData.append('file', file);
 
-    return this.http.post<CommonProject[]>(`${this.apiUrl}/upload`, formData);
+    return this.http.post<CommonProject[]>(`${environment.apiUrl}/api/EmployeeProject/upload`, formData);
   }
 }
