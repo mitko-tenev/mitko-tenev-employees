9fcd4cb0490c63176ce64b55c829105f2f6dc053
mitaka53 <34245881+mitaka53@users.noreply.github.com>
2025-04-26 13:23:35 +0300

    Update employee project controller logic

--- mitko-tenev-employees.Server/Controllers/EmployeeProjectController.cs
+++ mitko-tenev-employees.Server/Controllers/EmployeeProjectController.cs
@@ -2,15 +2,37 @@
 {
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
+    using mitko_tenev_employees.Server.Models;
+    using mitko_tenev_employees.Server.Services.Interfaces;
 
     [Route("api/[controller]")]
     [ApiController]
     public class EmployeeProjectController : ControllerBase
     {
+        private readonly ICsvParserService _csvParserService;
+        private readonly IEmployeeProjectService _employeeProjectService;
+
+        public EmployeeProjectController(ICsvParserService csvParserService, IEmployeeProjectService employeeProjectService)
+        {
+            this._csvParserService = csvParserService;
+            this._employeeProjectService = employeeProjectService;
+        }
+
         [HttpPost("upload")]
         public async Task<IActionResult> Upload(IFormFile file)
         {
-            return Ok();
+
+            try
+            {
+                List<EmployeeProject> parsedEmployeeProjects = this._csvParserService.ParseCsvFile(file);
+                var result = this._employeeProjectService.FindLongestWorkingPair(parsedEmployeeProjects);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Server error occurred: {ex.Message}");
+            }
         }
     }
 }
