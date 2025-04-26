126b7434e9d3b3c023c9f254cc5e4154ccf59588
mitaka53 <34245881+mitaka53@users.noreply.github.com>
2025-04-26 12:50:30 +0300

    Add EmployeeProjectController and unit test

---
+++ mitko-tenev-employees.Server/Controllers/EmployeeProjectController.cs
@@ -0,0 +1,16 @@
+﻿namespace mitko_tenev_employees.Server.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeProjectController : ControllerBase
+    {
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ mitko_tenev_employees.Server.Tests/Controllers/EmployeeProjectControllerTest.cs
@@ -0,0 +1,41 @@
+﻿namespace mitko_tenev_employees.Server.Tests.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using mitko_tenev_employees.Server.Controllers;
+    using NSubstitute;
+    using System.IO;
+    using FluentAssertions;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class EmployeeProjectControllerTest
+    {
+        private readonly EmployeeProjectController _sut;
+
+        public EmployeeProjectControllerTest()
+        {
+            this._sut = new EmployeeProjectController();
+        }
+
+        [Fact]
+        public async Task Upload()
+        {
+            var formFileMock = Substitute.For<IFormFile>();
+            var content = "test";
+            var fileName = "test.csv";
+
+            var ms = new MemoryStream();
+            var writer = new StreamWriter(ms);
+            writer.Write(content);
+            writer.Flush();
+
+            ms.Position = 0;
+            formFileMock.OpenReadStream().Returns(ms);
+            formFileMock.FileName.Returns(fileName);
+            formFileMock.Length.Returns(ms.Length);
+
+            var actual = await this._sut.Upload(formFileMock);
+
+            actual.Should().BeOfType<OkResult>();
+        }
+    }
+}
